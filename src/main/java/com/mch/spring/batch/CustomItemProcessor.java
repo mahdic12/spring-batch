22e663a9ec92e71896fc1e4d6d5666e2bf65fdc4
mahdi <mahdi@MCH.home>
2023-12-26 17:14:56 +0100

    [MCH]-Delete space

--- src/main/java/com/mch/spring/batch/CustomItemProcessor.java
+++ src/main/java/com/mch/spring/batch/CustomItemProcessor.java
@@ -4,14 +4,12 @@ import org.springframework.batch.item.ItemProcessor;
  
 import  com.mch.spring.batch.model.Report;
 /**
- * 
+ * Custom Processor to process data before writing in XML file
  * @author mahdi
- *
+ * (MCH)
  */
 public class CustomItemProcessor implements ItemProcessor<Report, Report> {
-
-	public Report process(Report item) throws Exception {
-		
+	public Report process(Report item) throws Exception {		
 		System.out.println("Processing..." + item);
 		String fname = item.getFirstName();
 		String lname = item.getLastName();
